8e37394b7ec96c1cdd3adb71b25a94cf70d2cfaf
Tanuv Nair <tanuvnair@gmail.com>
2023-12-06 18:41:52 +0530

    Added notes for operators in NOTES.md

--- playground.dart
+++ playground.dart
@@ -1,1 +1,40 @@
-main() {}
+import 'dart:io';
+
+main() {
+  print("Enter two numbers");
+  var num1String = stdin.readLineSync();
+  var num2String = stdin.readLineSync();
+
+  double num1 = double.parse(num1String!);
+  double num2 = double.parse(num2String!);
+
+  double add, subtract, multiply, divide;
+  add = num1 + num2;
+  subtract = num1 - num2;
+  multiply = num1 * num2;
+  divide = num1 / num2;
+
+  print("$add $subtract $multiply $divide");
+
+  // relational ==, !=, >=, <=
+  if (num1 % 2 == 0) {
+    print("Divisible by 2");
+  }
+
+  // unary operator
+  ++num1;
+  num1++;
+  num1 += 1;
+  num1 -= 1;
+  print(num1);
+
+  // logical && and logical ||
+  if (num1 > 9 && num1 < 100) {
+    print("$num1 is a 2 digit number");
+  }
+
+  // != Not equal
+  if (num1 != 100) {
+    print("$num1 is not equal to 100");
+  }
+}
